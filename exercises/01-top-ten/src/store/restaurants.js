70e131af4d0888000516ff11e633e2904953fe28
bcree11 <b_cree@yahoo.com>
2019-01-29 15:03:12 -0800

    Stretch Completed, add a Restaurant

--- exercises/01-top-ten/src/store/restaurants.js
+++ exercises/01-top-ten/src/store/restaurants.js
@@ -10,6 +10,12 @@
      name: 'Shenanigan\'s',
      location: 'Next to Teledyne',
      popular: ['Onion Bloom', 'Wings', 'Beer']
+   },
+   {
+     id: 3,
+     name: 'Monk\'s Cafe',
+     location: 'New York, New York',
+     popular: ['Tuna Fish Sandwich', 'Soup', 'Coffee']
    }
  ]
 
