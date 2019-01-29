e2151afd5b0f9d5beb8f7a1b0b9c9f16de9f1757
bcree11 <jhatch81@gmail.com>
2019-01-29 14:59:05 -0800

    Challenge Completed

--- exercises/01-top-ten/src/store/restaurants.js
+++ exercises/01-top-ten/src/store/restaurants.js
@@ -3,7 +3,7 @@
      id: 1,
      name: 'Bob\'s Burger',
      location: 'Near the wharf',
-     popular: ['New Bacon-ings', 'Coffee', 'Gene']
+     popular: ['New Bacon-ings', 'Coffee', 'Tina']
    },
    {
      id: 2,
